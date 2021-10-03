ca8bf6789122c6cb49dee4eb4292a877f6a086d8
Gabriel Fernandes <gabriel.work076@gmail.com>
2021-10-03 14:50:05 -0300

    bling service error handling

--- src/modules/bling/bling.service.ts
+++ src/modules/bling/bling.service.ts
@@ -11,16 +11,32 @@ export class BlingService {
   private blingApiKey: string = this.config.get('BLING_API_KEY');
 
   async findAllSellOrders(): Promise<any> {
-    return await lastValueFrom(
-      this.http.get(`${this.baseUrl}/pedidos/json?apikey=${this.blingApiKey}`),
-    );
+    try {
+      return await lastValueFrom(
+        this.http.get(
+          `${this.baseUrl}/pedidos/json?apikey=${this.blingApiKey}`,
+        ),
+      );
+    } catch (err) {
+      console.log(
+        'ERROR: An error occurred while trying to get all sell orders.',
+      );
+      throw err;
+    }
   }
 
   async createSellOrder(xmlOrder: string): Promise<any> {
-    return await lastValueFrom(
-      this.http.post(
-        `${this.baseUrl}/pedidos/json?apiKey=${this.blingApiKey}&xml=${xmlOrder}`,
-      ),
-    );
+    try {
+      return await lastValueFrom(
+        this.http.post(
+          `${this.baseUrl}/pedidos/json?apikey=${this.blingApiKey}&xml=${xmlOrder}`,
+        ),
+      );
+    } catch (err) {
+      console.log(
+        'ERROR: An error occurred while trying to create new sell order.',
+      );
+      throw err;
+    }
   }
 }
